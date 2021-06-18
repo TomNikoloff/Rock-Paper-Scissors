7ff2ea8eb7250c1e673b1c577723767687ffb6ef
TomNikoloff <tomnikoloff@hotmail.co.uk>
2021-06-18 19:34:44 +0100

    RPS Game Working
    Game is fully functioning. Reset button and counters still to be added.,

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,17 @@
 
 function rpsGame(yourChoice) {
     let humanChoice, botChoice;
-    //humanChoice = yourChoice.id;
-    //console.log(humanChoice);
+    humanChoice = yourChoice.id;
+
     botChoice = numberToChoice(randToRpsInt());
-    console.log(botChoice);
-    //results = decideWinner(humanChoice, botChoice);
-    //message = finalMessage(results);
-    //rpsFrontEnd(yourChoice.id, botChoice, message);
+    console.log('Computer Choice:', botChoice);
+
+    results = decideWinner(humanChoice, botChoice);
+    console.log(results);
+
+    message = finalMessage(results);
+    console.log(message);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
 }
 
 function randToRpsInt() {
@@ -16,4 +20,49 @@ function randToRpsInt() {
 
 function numberToChoice(number) {
     return ['rock', 'paper', 'scissors'][number]
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    let rpsDatabase = {
+        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+        'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0},
+    }
+
+    let yourScore = rpsDatabase[yourChoice][computerChoice];
+    let computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return {'message': 'You Lost!', 'color': '#8c52ff'};
+    } else if (yourScore === 0.5) {
+        return {'message': 'You tied!', 'color': '#8c52ff'};
+    } else {
+        return {'message': 'You Won!', 'color': '#8c52ff'};
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    let imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    }
+
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    let humanDiv = document.getElementById('humanDiv');
+    let botDiv = document.getElementById('botDiv');
+    let messageDiv = document.getElementById('messageDiv');
+
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "'>";
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "'>";
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding-top: 50%; '>" + finalMessage['message'] + "</h1>";
+
 }
